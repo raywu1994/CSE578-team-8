1e3fc89ac3c31e7d943173649fb6bfda292b548e
rwu864 <rwu864@gmail.com>
2019-06-23 13:47:04 -0700

    Generated some more stacked bar charts

--- visualizations.py
+++ visualizations.py
@@ -6,22 +6,34 @@ def main():
     df = load_data()
 
     generateVsEarningsStackedBarChart(df, 'Age')
-
+    generateVsEarningsStackedBarChart(df, 'Education')
+    generateVsEarningsStackedBarChart(df, 'Martial_Status')
+    generateVsEarningsStackedBarChart(df, 'Occupation', figSizeWidth = 25, figSizeLength = 10, xticksRotation = 25)
+    generateVsEarningsStackedBarChart(df, 'Relationship')
+    generateVsEarningsStackedBarChart(df, 'Race')
+    generateVsEarningsStackedBarChart(df, 'Gender')
+    generateVsEarningsStackedBarChart(df, 'From_USA')
+    
 def load_data():
     #read in both datasets, combine into one
     training_data = pd.read_csv('adult.csv')
     headers = training_data.columns.values.tolist()
     test_data = pd.read_csv('adult.test.csv', names=headers)
     all_data = pd.concat([training_data, test_data])
-    all_data['Below50k'] = all_data['Salarys'].apply(lambda x: False if x == ' >50K' else True)
+
+    #set some calculated fields
+    all_data['Below_50k'] = all_data['Salarys'].apply(lambda x: False if x == ' >50K' else True)
+    all_data['From_USA'] = all_data['NTVCTRY'].apply(lambda x: True if x == ' United-States' else False)
+
     return all_data
 
-def generateVsEarningsStackedBarChart(df, columnName, figSizeWidth = 20, figSizeLength = 5, barWidth = 0.5):
-    df = df[[columnName, 'Below50k']]
+def generateVsEarningsStackedBarChart(df, columnName, figSizeWidth = 20, figSizeLength = 5, barWidth = 0.5,  xticksRotation = None):
+    df = df[[columnName, 'Below_50k']]
 
-    above50kByAge = df.loc[df['Below50k'] == False].groupby(columnName).count().rename(columns={'Below50k': 'CountOfAbove50k'})
-    below50kByAge = df.loc[df['Below50k'] == True].groupby(columnName).count().rename(columns={'Below50k': 'CountOfBelow50k'})
-    result = pd.merge(above50kByAge, below50kByAge,how='outer', on=columnName).sort_values(by=[columnName]).reset_index()
+    above50kByAge = df.loc[df['Below_50k'] == False].groupby(columnName).count().rename(columns={'Below_50k': 'CountOfAbove50k'})
+    below50kByAge = df.loc[df['Below_50k'] == True].groupby(columnName).count().rename(columns={'Below_50k': 'CountOfBelow50k'})
+    result = pd.merge(above50kByAge, below50kByAge,how='outer', on=columnName)
+    result = result.sort_values(by=[columnName]).reset_index()
 
     bars1 = result['CountOfBelow50k'].values
     bars2 = result['CountOfAbove50k'].values
@@ -29,14 +41,14 @@ def generateVsEarningsStackedBarChart(df, columnName, figSizeWidth = 20, figSize
 
     r = np.arange(len(result.index))
 
-    plt.figure(figsize=(20,5))
+    plt.figure(figsize=(figSizeWidth, figSizeLength))
     p1 = plt.bar(r, bars1, color='#7f6d5f', edgecolor='white', width=barWidth)
     p2 = plt.bar(r, bars2, bottom=bars1, color='#557f2d', edgecolor='white', width=barWidth)
 
     plt.legend((p1[0], p2[0]), ('<=50k', '>50k'), loc='upper right')
     plt.title(f'{columnName} vs Earnings', weight='bold')
     plt.ylabel('Count', weight='bold')
-    plt.xticks(r, columnValues)
+    plt.xticks(r, columnValues, rotation = xticksRotation)
     plt.xlabel(columnName, weight='bold')
 
     plt.savefig(f'vis/{columnName}_vs_earnings.png')
