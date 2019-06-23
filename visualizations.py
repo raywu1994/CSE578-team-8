9a96399e705eda8a773a6e22144b090d48ed0418
rwu864 <rwu864@gmail.com>
2019-06-23 13:14:10 -0700

    refactor

--- visualizations.py
+++ visualizations.py
@@ -4,44 +4,42 @@ import matplotlib.pyplot as plt
 
 def main():
     df = load_data()
-    df['Below50k'] = df['Salarys'].apply(lambda x: False if x == ' >50K' else True)
 
-    columns_of_interest = ['Age','Below50k']
-    coi_df = df[columns_of_interest]
-
-    above50kByAge = coi_df.loc[coi_df['Below50k'] == False].groupby('Age').count().rename(columns={'Below50k': 'CountOfAbove50kByAge'})
-    below50kByAge = coi_df.loc[coi_df['Below50k'] == True].groupby('Age').count().rename(columns={'Below50k': 'CountOfBelow50kByAge'})
-
-    result = pd.merge(above50kByAge, below50kByAge,how='outer', on='Age').sort_values(by=['Age']).reset_index()
-    print(result)
-
-    bars1 = result['CountOfBelow50kByAge'].values
-    bars2 = result['CountOfAbove50kByAge'].values
-    ages = result['Age'].values
-
-    r = np.arange(len(result.index))
-    barWidth = 0.5
-
-    plt.figure(figsize=(20,5))
-    p1 = plt.bar(r, bars1, color='#7f6d5f', edgecolor='white', width=barWidth)
-    p2 = plt.bar(r, bars2, bottom=bars1, color='#557f2d', edgecolor='white', width=barWidth)
-
-    plt.legend((p1[0], p2[0]), ('<=50k', '>50k'), loc='upper right')
-    plt.title('Age vs Earnings')
-    plt.ylabel('Count')
-    plt.xticks(r, ages)
-    plt.xlabel('Age')
-
-
-    plt.savefig('vis/age_vs_earnings.png')
-    # plt.show()
+    generateVsEarningsStackedBarChart(df, 'Age')
 
 def load_data():
     #read in both datasets, combine into one
     training_data = pd.read_csv('adult.csv')
     headers = training_data.columns.values.tolist()
     test_data = pd.read_csv('adult.test.csv', names=headers)
-    return pd.concat([training_data, test_data])
+    all_data = pd.concat([training_data, test_data])
+    all_data['Below50k'] = all_data['Salarys'].apply(lambda x: False if x == ' >50K' else True)
+    return all_data
+
+def generateVsEarningsStackedBarChart(df, columnName, figSizeWidth = 20, figSizeLength = 5, barWidth = 0.5):
+    df = df[[columnName, 'Below50k']]
+
+    above50kByAge = df.loc[df['Below50k'] == False].groupby(columnName).count().rename(columns={'Below50k': 'CountOfAbove50k'})
+    below50kByAge = df.loc[df['Below50k'] == True].groupby(columnName).count().rename(columns={'Below50k': 'CountOfBelow50k'})
+    result = pd.merge(above50kByAge, below50kByAge,how='outer', on=columnName).sort_values(by=[columnName]).reset_index()
+
+    bars1 = result['CountOfBelow50k'].values
+    bars2 = result['CountOfAbove50k'].values
+    columnValues = result[columnName].values
+
+    r = np.arange(len(result.index))
+
+    plt.figure(figsize=(20,5))
+    p1 = plt.bar(r, bars1, color='#7f6d5f', edgecolor='white', width=barWidth)
+    p2 = plt.bar(r, bars2, bottom=bars1, color='#557f2d', edgecolor='white', width=barWidth)
+
+    plt.legend((p1[0], p2[0]), ('<=50k', '>50k'), loc='upper right')
+    plt.title(f'{columnName} vs Earnings')
+    plt.ylabel('Count')
+    plt.xticks(r, columnValues)
+    plt.xlabel(columnName)
+
+    plt.savefig(f'vis/{columnName}_vs_earnings.png')
 
 if __name__ == "__main__":
     main()
